90482bda36f0cdd7897cf730f8fdf9782d7946aa
Zenitsu-Sumeet <113230293+Zenitsu-Sumeet@users.noreply.github.com>
2023-02-10 11:16:52 +0530

    THFGH

---
+++ PYTHON/SQAURE.py
@@ -0,0 +1,14 @@
+import turtle
+
+t=turtle.Turtle()
+s=100
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+
+turtle.done()
---
+++ PYTHON/spiral.py
@@ -0,0 +1,6 @@
+import turtle
+
+t= turtle.Turtle()
+r=10
+for i in range(100):
+   t.circle(r*i,45)
